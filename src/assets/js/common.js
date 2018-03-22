e26058e41088bac909d751530e5bbab550186ec9
wubo <593261671@qq.com>
2018-03-22 17:34:56 +0800

    task322

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,1 +1,10 @@
 import '../css/main.scss';
+import $ from 'jquery';
+//折叠导航栏
+$('.j-nav>li').on('click',function(){
+  if($(this).is('.open')){
+    $(this).removeClass('open');
+  }else{
+    $(this).addClass('open');
+  }
+})
