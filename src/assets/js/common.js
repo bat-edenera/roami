66a138fd5444b4e212272d5cf80ffb4c83cdfe23
unknown <593261671@qq.com>
2018-03-26 20:34:30 +0800

    'init'

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,4 +1,1 @@
-import '../css/weygo.css';
-import '../css/reset.css';
 import '../css/main.scss';
-console.log('common')
--- src/assets/js/index.js
+++
@@ -1,2 +0,0 @@
-var html = require('../../view/index.html');
-console.log('index 欢迎你')
