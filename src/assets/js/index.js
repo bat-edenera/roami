6af789167b7429dcfe6f45bcaadb54056cb6ba2b
wubo <593261671@qq.com>
2018-04-03 09:38:19 +0800

    init

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,13 @@
 import '../css/main.scss';
-import swiper from 'swiper';
-$(function(){
-  console.log('index')
+import Swiper from 'swiper';
+import '../css/swiper.css';
+
+var indexSwiper = new Swiper('.j-mainSwiper', {
+  loop:true,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  pagination: {
+    el: '.j-mainSwiper .swiper-pagination',
+    clickable: true,
+  },
 })
