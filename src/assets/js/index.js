15bbad15eb08df16807a4690b65ed9814921ea66
unknown <593261671@qq.com>
2018-04-09 22:11:31 +0800

    'init'

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,14 @@
 import '../css/main.scss';
 // import swiper from 'swiper';
 $(function(){
+  $('.j-tab').on('click','li',function(){
+    if($(this).is('.active')){
+      return;
+    }
+    var i = $(this).index();
+    $(this).siblings().removeClass('active').end().addClass('active');
+
+    $('.j-list li').removeClass('active').eq(i).addClass('active');
+    
+  })
 })
