fce9b0e36268fdbb9ee423da008ef22fca6038d6
unknown <593261671@qq.com>
2018-04-20 22:48:48 +0800

    'animal'

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 import '../css/main.scss';
-// import swiper from 'swiper';
+import Swiper from 'swiper';
 $(function(){
+  $('.j-list').addClass('active');
   $('.j-tab').on('click','li',function(){
     if($(this).is('.active')){
       return;
@@ -8,7 +9,32 @@ $(function(){
     var i = $(this).index();
     $(this).siblings().removeClass('active').end().addClass('active');
 
-    $('.j-list li').removeClass('active').eq(i).addClass('active');
+    $('.j-list').removeClass('active').eq(i).addClass('active');
+    if(i==1){
+      $('.j-wanpi').addClass('active');
+    }else{
+      $('.j-wanpi').removeClass('active');
+    }
     
   })
+
+  var vedioSwiper = new Swiper('.j-vedioSwiper', {
+    navigation: {
+      nextEl: '.vedio-list .swiper-button-next',
+      prevEl: '.vedio-list .swiper-button-prev',
+    },
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 25,
+  });
+  var picSwiper = new Swiper('.j-picSwiper', {
+    navigation: {
+      nextEl: '.pic-list .swiper-button-next',
+      prevEl: '.pic-list .swiper-button-prev',
+    },
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 60,
+  });
+  $('.j-list').removeClass('active').eq(0).addClass('active');
 })
