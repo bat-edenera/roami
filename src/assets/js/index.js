0f811b009ad5f5f7e582bb06886285f16bf42123
unknown <jack_wu@alldone.com.cn>
2018-10-08 17:35:34 +0800

    init

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,3 +7,13 @@ $(function(){
     loop: true
   })
 })
+
+
+// import '../css/main.scss';
+// import Swiper from 'swiper/dist/js/swiper';
+// $(function () {
+//   var mySwiper = new Swiper('.swiper-container', {
+//     autoplay: true,//可选选项，自动滑动
+//     loop: true
+//   })
+// })
--- src/assets/js/mindex.js
+++
@@ -1,8 +0,0 @@
-import '../css/main.scss';
-import Swiper from 'swiper/dist/js/swiper';
-$(function () {
-  var mySwiper = new Swiper('.swiper-container', {
-    autoplay: true,//可选选项，自动滑动
-    loop: true
-  })
-})
