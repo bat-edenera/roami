eccf3c3235f3dd3d7669859b8774611e0c82fb80
effile <593261671@qq.com>
2018-10-25 23:00:09 +0800

    done wine2

--- src/assets/js/wine.2.js
+++ src/assets/js/wine.2.js
@@ -1,19 +1,39 @@
 import '../css/main.scss';
 import Swiper from 'swiper/dist/js/swiper';
 $(function () {
- new Swiper('.swiper-container', {
-    // autoplay: true,//可选选项，自动滑动
-    loop: true,
-    effect: 'fade',
-    fadeEffect: true,
-    speed: 1000,
-    // height:500,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  var thumbsSwiper = new Swiper('.swiper-thumb',{
+    spaceBetween: 15,
+    slidesPerView: 5,
+    watchSlidesVisibility: true,//防止不可点击
+  })
+  new Swiper('.swiper-main',{
+    spaceBetween: 10,
+    thumbs: {
+      swiper: thumbsSwiper,
+    }
+  })
+  $('.j-cate').on('click','li',function() {
+    $(this).siblings().removeClass('active').end()
+    .addClass('active');
+  })
+//nums
+  $('.j-nums').on('click',function(){
+    var $input = $(this).siblings('input');
+    var num = +$input.val();
+    if($(this).is('.plus')){
+      num++
+    }else {
+      num--
+    }
+    num == 0 && (num = 1)
+    $input.val(num);
+  })
+  //nav
+  $('.j-nav').on('click','li',function() {
+    $(this).siblings().removeClass('active').end()
+    .addClass('active');
+    var i = $(this).index();
+    console.log(i)
+    $('.j-list>li').removeClass('active').eq(i).addClass('active');
   })
 })
